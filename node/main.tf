b6f64e949ada2749504e457ad10ee18ab6dabcc0
Mike Petersen <mike.petersen@loft.sh>
2025-10-20 14:40:30 -0700

    fix formatting

--- node/main.tf
+++ node/main.tf
@@ -24,6 +24,19 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vms" {
   name      = local.vm_name
   node_name = "pve"
 
+  initialization {
+
+    user_account {
+      username = "ubuntu"
+      # Replace this with SSH Key
+      password = "ubuntu"
+    }
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+  }
 
   disk {
     datastore_id = "local-lvm"
@@ -36,20 +49,5 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vms" {
 
   network_device {
     bridge = "vmbr0"
-  }
-
-  initialization {
-
-    user_account {
-      username = "ubuntu"
-      # Replace this with SSH Key
-      password = "ubuntu"
-
-      ip_config {
-        ipv4 {
-          address = "dhcp"
-        }
-      }
-    }
   }
 }
