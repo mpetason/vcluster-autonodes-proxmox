38293f8afdc2f06b73bd730af19c965923812385
Mike Petersen <mike.petersen@loft.sh>
2025-10-23 08:29:32 -0700

    update naming

--- node/main.tf
+++ node/main.tf
@@ -45,7 +45,7 @@ resource "proxmox_virtual_environment_file" "user_data_cloud_config" {
 
 resource "proxmox_virtual_environment_vm" "ubuntu_vms" {
 
-  name      = "my-vm-${random_string.vm_name_suffix.result}"
+  name      = "${var.vcluster.nodeClaim.metadata.name}-${random_string.vm_name_suffix.result}"
   node_name = "pve"
 
   initialization {
